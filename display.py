d235c34362a9acc8d68ae3f00132e70dce5d66b1
Brett Clark <breclark@zendesk.com>
2019-05-22 09:06:27 +1000

    Clean up Display

--- display.py
+++ display.py
@@ -3,6 +3,12 @@ import pdb
 
 class Display:
     def __init__(self, maze, square_size=30):
+        """Constructs the maze display.
+
+        Arguments:
+            maze -- the Maze to show.
+            square_size -- the width of each square in the Maze.
+        """
         self.maze = maze
         self.square_size = square_size
         self.origin = maze.dim * square_size / -2
@@ -26,6 +32,8 @@ class Display:
 
     def draw_maze(self):
         """Draws the maze structure.
+
+        Animation is turned off for this step, so it will happen instantaneously. 
         """
         # Turn animation off to draw maze instantaneously.
         self.screen.tracer(0)
@@ -58,6 +66,7 @@ class Display:
             cell -- the position of the cell.
             side -- the heading of the wall.
         """
+        # Get the starting point and direction for the line.
         start = None
         heading = None
         if side == self.maze.NORTH:
@@ -76,6 +85,7 @@ class Display:
             start = (self.origin + self.square_size * cell[0], self.origin + self.square_size * cell[1])
             heading = self.maze.NORTH
 
+        # Draw the line.
         self.maze_tool.penup()
         self.maze_tool.goto(*start)
         self.maze_tool.setheading(heading)
@@ -84,13 +94,11 @@ class Display:
         self.maze_tool.penup()
 
     def place_mouse(self, pos, heading):
-        """places the mouse in the maze.
+        """Places the mouse in the maze.
 
-        arguments:
-            pos -- the position to place. a tuple containing position of mouse
-                on board from 0 to maze (dim - 1).
-            heading -- the heading the mouse has. a value in degrees from 0
-                (right) to 360, counter-clockwise.
+        Arguments:
+            pos -- the mouse's position. a list containing x, y integer co-ordinates. 
+            heading -- the mouse's heading. an integer in the range (0, 360].
         """
 
         # Set the mouse location.
@@ -106,28 +114,57 @@ class Display:
         self.mouse_tool.pendown()
 
     def set_heading(self, heading):
+        """Draws a new heading for the mouse.
+
+        Arguments:
+            heading -- the mouse's heading. an integer in the range (0, 360].
+        """
         # Set the mouse's heading.
         self.mouse_tool.setheading(heading)
 
     def move(self, pos):
+        """Draws a mouse's move.
+
+        Arguments:
+            pos -- the mouse's position. a list containing x, y integer co-ordinates. 
+        """
         # Get x, y coordinates.
         x = self.origin + (pos[0] + 0.5) * self.square_size
         y = self.origin + (pos[1] + 0.5) * self.square_size
         self.mouse_tool.goto(x, y)
 
     def clear_track(self):
+        """Clears the mouse's tracks from the display.
+        """
         self.mouse_tool.clear()
 
     def mainloop(self):
+        """Begins the Turtle mainloop.
+
+        This loop can only be broken using the 'Display.close()' method.
+        """
         turtle.mainloop()
         
     def close(self):
+        """Closes the display.
+        """
         turtle.bye()
 
     def on_space(self, callback):
+        """Registers a callback to run on 'space' event on display.
+
+        Arguments:
+            callback -- a function to run on 'space' press.
+        """
         self.screen.onkey(callback, 'space')
 
     def sleep(self, callback, delay):
+        """Registers a callback to run after some delay.
+
+        Arguments:
+            callback -- the function to run after delay.
+            delay -- the delay in ms.
+        """
         self.screen.ontimer(callback, delay)
 
 
